232747204ab51c350593a06d8c2c4895963aecd0
Selcuk <Selcuk@SK18>
2024-09-14 19:26:08 +0300

    Add project files.

---
+++ SingletonDesignPattern/Logger.cs
@@ -0,0 +1,37 @@
+﻿namespace SingletonDesignPattern;
+
+public class Logger
+{
+    // 1. Statik bir örnek oluştur.
+    private static Logger _instance;
+
+    // 2. Erişim kilidi (thread safety için)
+    private static readonly object _lock = new object();
+    
+    // 3. Constructor'ı private yaparak dışarıdan nesne oluşturmayı engelle.
+    private Logger()
+    {
+
+    }
+    // 4. Singleton instance'ı almak için bir metod tanımla.
+    public static Logger Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+    }
+
+    // 5. Log yazma metodu.
+    public void Log(string message)
+    {
+        Console.WriteLine(message);
+    }
+}
---
+++ SingletonDesignPattern/Program.cs
@@ -0,0 +1,18 @@
+﻿using SingletonDesignPattern;
+
+#region Singleton Design Pattern
+//Singleton deseni, bir sınıfın yalnızca bir örneğinin oluşturulmasını garanti altına almak için kullanılır.
+//Bu desen, global erişime izin verirken bir sınıfın yalnızca bir kez oluşturulmasını sağlar.
+//Konfigürasyon Ayarlarının bir yerde tutulması
+//Log işlemlerinin tek bir sınıf üzerinden yapılması
+//veritabanı bağlantılarının merkezi bir yerde yönetilmesi
+//global erişim imkanı sağlaması
+//çoklu thread kullanımında dikkatli olmak gerekir.
+Logger logger  = Logger.Instance;
+logger.Log("First Log");
+
+Logger anotherLogger = Logger.Instance;
+anotherLogger.Log("Second Log");
+// İki logger nesnesi de aynı instance'dır.
+Console.WriteLine(Object.ReferenceEquals(logger, anotherLogger)); // True dönecektir
+#endregion
